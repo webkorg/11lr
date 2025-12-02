77e7d4d1e47bcf42a92ea71d48bf910ae3544ef2
webkorg <web.korg@gmail.com>
2025-12-02 16:21:40 +0200

    add changes

--- 11lr/11lr.cpp
+++ 11lr/11lr.cpp
@@ -35,29 +35,5 @@ int main()
     }
     cout << endl;
 
-    for (int i = 0; i < n; i++) {
-
-        for (int j = 0; j < m; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < n; i++) {
-
-        for (int j = 0; j < m; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < n; i++) {
-
-        for (int j = 0; j < m; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
 
